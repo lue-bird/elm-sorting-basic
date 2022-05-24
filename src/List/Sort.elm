b83856c7e5cbb2b53423fafe8c97912096ba289d
lue-bird <lue.the.bird@gmail.com>
2022-05-24 10:49:17 +0200

    + `List.Sort.bubble` & `insertion` & `merge` & `quick` +

---
+++ src/List/Sort.elm
@@ -0,0 +1,154 @@
+module List.Sort exposing (bubble, each2OrderMerge, insertion, merge, quick)
+
+import Order exposing (Ordering)
+
+
+bubble : Ordering element -> List element -> List element
+bubble elementOrder =
+    \list ->
+        case list |> bubbleStep elementOrder of
+            [] ->
+                []
+
+            head :: tail ->
+                head :: (tail |> bubble elementOrder)
+
+
+bubbleStep : Ordering element -> List element -> List element
+bubbleStep elementOrder =
+    \list ->
+        case list of
+            [] ->
+                []
+
+            [ onlyElement ] ->
+                [ onlyElement ]
+
+            leftHead :: rightHead :: listFrom2 ->
+                let
+                    listFrom2Sorted =
+                        listFrom2 |> bubbleStep elementOrder
+                in
+                case elementOrder leftHead rightHead of
+                    LT ->
+                        leftHead :: rightHead :: listFrom2Sorted
+
+                    EQ ->
+                        leftHead :: rightHead :: listFrom2Sorted
+
+                    GT ->
+                        rightHead :: leftHead :: listFrom2Sorted
+
+
+insertion : Ordering element -> List element -> List element
+insertion elementOrder =
+    List.foldl
+        (\element insertionList ->
+            (element :: insertionList) |> bubble elementOrder
+        )
+        []
+
+
+quick : Ordering element -> List element -> List element
+quick elementOrder =
+    \list ->
+        case list of
+            [] ->
+                []
+
+            head :: tail ->
+                let
+                    ( before, after ) =
+                        tail
+                            |> List.partition
+                                (\tailElement ->
+                                    elementOrder tailElement head == LT
+                                )
+                in
+                (before |> quick elementOrder)
+                    ++ (head :: (after |> quick elementOrder))
+
+
+merge : Ordering element -> List element -> List element
+merge elementOrder =
+    \list ->
+        list
+            |> List.indexedMap Tuple.pair
+            |> mergeHelp
+                { elementOrder = elementOrder
+                , length = list |> List.length
+                }
+
+
+{-| Anyone an idea why this leads to a
+
+> RangeError: Maximum call stack size exceeded
+
+-}
+mergeHelp :
+    { elementOrder : Ordering element
+    , length : Int
+    }
+    -> List ( Int, element )
+    -> List element
+mergeHelp { elementOrder, length } =
+    \list ->
+        case list of
+            [] ->
+                []
+
+            [ ( _, onlyElement ) ] ->
+                [ onlyElement ]
+
+            element0 :: element1 :: listFrom2 ->
+                let
+                    rightLength =
+                        length // 2
+
+                    leftLength =
+                        length - rightLength
+
+                    ( left, right ) =
+                        (element0 :: element1 :: listFrom2)
+                            |> List.partition
+                                (\( index, _ ) -> index <= leftLength - 1)
+
+                    leftSorted =
+                        left |> mergeHelp { elementOrder = elementOrder, length = leftLength }
+
+                    rightSorted =
+                        right |> mergeHelp { elementOrder = elementOrder, length = rightLength }
+                in
+                ( leftSorted, rightSorted ) |> each2OrderMerge elementOrder
+
+
+each2OrderMerge :
+    Ordering element
+    -> ( List element, List element )
+    -> List element
+each2OrderMerge elementOrder =
+    \listsLeftRight ->
+        case listsLeftRight of
+            ( [], [] ) ->
+                []
+
+            ( [], rightHead :: rightTail ) ->
+                rightHead :: rightTail
+
+            ( leftHead :: leftTail, [] ) ->
+                leftHead :: leftTail
+
+            ( leftHead :: leftTail, rightHead :: rightTail ) ->
+                let
+                    tailsEach2OrderMerged =
+                        ( leftTail, rightTail ) |> each2OrderMerge elementOrder
+                in
+                case elementOrder leftHead rightHead of
+                    LT ->
+                        leftHead :: rightHead :: tailsEach2OrderMerged
+
+                    EQ ->
+                        leftHead :: rightHead :: tailsEach2OrderMerged
+
+                    GT ->
+                        rightHead :: leftHead :: tailsEach2OrderMerged
---
+++ tests/List/Sort/Test.elm
@@ -0,0 +1,68 @@
+module List.Sort.Test exposing (suite)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Html.Attributes exposing (name)
+import List.Sort
+import Order
+import Test exposing (Test, describe, test)
+
+
+suite : Test
+suite =
+    let
+        orderByNameString =
+            Order.by ( .name, Order.string { case_ = Order.lowerUpper } )
+    in
+    describe
+        "sort basic"
+        [ Test.fuzz
+            (Fuzz.list (Fuzz.map (\name -> { name = name }) Fuzz.string))
+            "quick"
+            (\list ->
+                list
+                    |> List.Sort.quick orderByNameString
+                    |> Expect.equalLists
+                        (list |> List.sortWith orderByNameString)
+            )
+        , Test.fuzz
+            (Fuzz.list (Fuzz.map (\name -> { name = name }) Fuzz.string))
+            "merge"
+            (\list ->
+                [ { name = "A" }, { name = "B" }, { name = "C" }, { name = "D" } ]
+                    |> List.Sort.merge orderByNameString
+                    |> Expect.equalLists
+                        ([ { name = "A" }, { name = "B" }, { name = "C" }, { name = "D" } ]
+                            |> List.sortWith orderByNameString
+                        )
+            )
+        , test
+            "each2OrderMerge"
+            (\() ->
+                ( [ 1, 8, 61 ], [ 2, 7, 13 ] )
+                    |> List.Sort.each2OrderMerge compare
+                    |> Expect.equalLists [ 1, 2, 7, 8, 13, 61 ]
+            )
+        , Test.fuzz
+            (Fuzz.list (Fuzz.map (\name -> { name = name }) Fuzz.string))
+            "bubble"
+            (\list ->
+                [ { name = "A" }, { name = "B" }, { name = "C" }, { name = "D" } ]
+                    |> List.Sort.bubble orderByNameString
+                    |> Expect.equalLists
+                        ([ { name = "A" }, { name = "B" }, { name = "C" }, { name = "D" } ]
+                            |> List.sortWith orderByNameString
+                        )
+            )
+        , Test.fuzz
+            (Fuzz.list (Fuzz.map (\name -> { name = name }) Fuzz.string))
+            "insertion"
+            (\list ->
+                [ { name = "A" }, { name = "B" }, { name = "C" }, { name = "D" } ]
+                    |> List.Sort.insertion orderByNameString
+                    |> Expect.equalLists
+                        ([ { name = "A" }, { name = "B" }, { name = "C" }, { name = "D" } ]
+                            |> List.sortWith orderByNameString
+                        )
+            )
+        ]
