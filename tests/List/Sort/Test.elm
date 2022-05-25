b164c8f88096fa20dc204a9375832508cf1eee6c
lue-bird <lue.the.bird@gmail.com>
2022-05-25 11:43:58 +0200

    `List.Sort.Test` group descriptions +-

--- tests/List/Sort/Test.elm
+++ tests/List/Sort/Test.elm
@@ -5,7 +5,7 @@ import Fuzz exposing (Fuzzer, int, list, string)
 import Html.Attributes exposing (name)
 import List.Sort
 import Order
-import Test exposing (Test, describe, test)
+import Test exposing (Test, test)
 
 
 suite : Test
@@ -14,8 +14,8 @@ suite =
         orderByNameString =
             Order.by ( .name, Order.string { case_ = Order.lowerUpper } )
     in
-    describe
-        "sort basic"
+    Test.describe
+        "`List` sort basic"
         [ Test.fuzz
             (Fuzz.list (Fuzz.map (\name -> { name = name }) Fuzz.string))
             "quick"
@@ -25,24 +25,27 @@ suite =
                     |> Expect.equalLists
                         (list |> List.sortWith orderByNameString)
             )
-        , Test.fuzz
-            (Fuzz.list (Fuzz.map (\name -> { name = name }) Fuzz.string))
+        , Test.describe
             "merge"
-            (\list ->
-                [ { name = "A" }, { name = "B" }, { name = "C" }, { name = "D" } ]
-                    |> List.Sort.merge orderByNameString
-                    |> Expect.equalLists
-                        ([ { name = "A" }, { name = "B" }, { name = "C" }, { name = "D" } ]
-                            |> List.sortWith orderByNameString
-                        )
-            )
-        , test
-            "each2OrderMerge"
-            (\() ->
-                ( [ 1, 8, 61 ], [ 2, 7, 13 ] )
-                    |> List.Sort.each2OrderMerge compare
-                    |> Expect.equalLists [ 1, 2, 7, 8, 13, 61 ]
-            )
+            [ Test.fuzz
+                (Fuzz.list (Fuzz.map (\name -> { name = name }) Fuzz.string))
+                "full"
+                (\list ->
+                    [ { name = "A" }, { name = "B" }, { name = "C" }, { name = "D" } ]
+                        |> List.Sort.merge orderByNameString
+                        |> Expect.equalLists
+                            ([ { name = "A" }, { name = "B" }, { name = "C" }, { name = "D" } ]
+                                |> List.sortWith orderByNameString
+                            )
+                )
+            , test
+                "each2OrderMerge"
+                (\() ->
+                    ( [ 1, 8, 61 ], [ 2, 7, 13 ] )
+                        |> List.Sort.each2OrderMerge compare
+                        |> Expect.equalLists [ 1, 2, 7, 8, 13, 61 ]
+                )
+            ]
         , Test.fuzz
             (Fuzz.list (Fuzz.map (\name -> { name = name }) Fuzz.string))
             "bubble"
